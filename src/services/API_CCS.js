20c7a2225e8730d37479290671167236ac8e71bf
DanielDO93 <61757109+DanielDO93@users.noreply.github.com>
2020-07-27 12:17:15 -0500

    Update API_CCS.js

--- src/services/API_CCS.js
+++ src/services/API_CCS.js
@@ -1,8 +1,8 @@
 import AuthService from "./AuthService";
 
 //Estas son las lineas que debes switchear cuando estes haciendo tus pruebas
-//const hostURL = "https://api.ccscontactcenter.com";
-const hostURL = 'http://localhost:8082'
+const hostURL = "https://api.ccscontactcenter.com";
+//const hostURL = 'http://localhost:8082'
 
 export default class API_CCS {
   constructor() {
